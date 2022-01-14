3e383b64e2447b462783d45783c934a05d310b89
Devous <43449531+Devous@users.noreply.github.com>
2022-01-14 01:11:04 +0000

    Minor changes

--- src/main/java/dev/devous/electron/Electron.java
+++ src/main/java/dev/devous/electron/Electron.java
@@ -7,9 +7,14 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.ScheduledExecutorService;
 
 public final class Electron {
+    private final @NotNull PacketQueue packetQueue;
     public Electron(final @NotNull MongoCollection<Document> collection, final @NotNull PacketHandler packetHandler,
                     final @NotNull ScheduledExecutorService scheduledExecutorService) {
-        PacketQueue packetQueue = new PacketQueue(collection);
+        packetQueue = new PacketQueue(collection);
         new PacketListener(packetQueue, packetHandler, scheduledExecutorService).subscribe();
+    }
+
+    public @NotNull PacketQueue packetQueue() {
+        return packetQueue;
     }
 }
